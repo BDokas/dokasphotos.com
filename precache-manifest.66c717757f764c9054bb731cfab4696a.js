f4965989f0a3e150bc6768bafcb54b2f2d562a54
Brian <brianadokas@gmail.com>
2021-04-18 11:47:15 -0400

    Updates

---
+++ precache-manifest.66c717757f764c9054bb731cfab4696a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4c3e8122d60fff948d03ff1830d101",
+    "url": "/index.html"
+  },
+  {
+    "revision": "789f2092f758256fb8f3",
+    "url": "/static/css/main.8f954d81.chunk.css"
+  },
+  {
+    "revision": "030bd15aea44aeded9d1",
+    "url": "/static/js/2.2a132397.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2a132397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "789f2092f758256fb8f3",
+    "url": "/static/js/main.df4ad768.chunk.js"
+  },
+  {
+    "revision": "bb48338029eaf81ac017",
+    "url": "/static/js/runtime-main.3991bc28.js"
+  },
+  {
+    "revision": "0325d23cd98b1cf0adcfea37176e700c",
+    "url": "/static/media/assembly.0325d23c.jpg"
+  },
+  {
+    "revision": "4d70e7904a34b01fd0804f6858d4507b",
+    "url": "/static/media/biography.4d70e790.jpg"
+  },
+  {
+    "revision": "ad246fd20d9d4c6e4b5654aa6f93f758",
+    "url": "/static/media/burning_mask.ad246fd2.jpg"
+  },
+  {
+    "revision": "c19c1af97d6aed6360087b77659352a9",
+    "url": "/static/media/deardorff.c19c1af9.jpg"
+  },
+  {
+    "revision": "9f7ea2e3335029c224effe66b65e2d48",
+    "url": "/static/media/diffusion_plastic.9f7ea2e3.jpg"
+  },
+  {
+    "revision": "04484492499c68ea2a33cd9da56b90ca",
+    "url": "/static/media/dodging_mask.04484492.jpg"
+  },
+  {
+    "revision": "6f61b2bb32492970412372616f5ed5ca",
+    "url": "/static/media/enlarger_bottom.6f61b2bb.jpg"
+  },
+  {
+    "revision": "7c4aaffeee66d8252537d5fd3ee332a8",
+    "url": "/static/media/enlarger_front.7c4aaffe.jpg"
+  },
+  {
+    "revision": "905526d1362b13551df510017ecaae08",
+    "url": "/static/media/enlarger_left.905526d1.jpg"
+  },
+  {
+    "revision": "8ea85c9d0974369fee2a20ca5bded001",
+    "url": "/static/media/enlarger_right.8ea85c9d.jpg"
+  },
+  {
+    "revision": "70a4c00746f1fe75128ddc03214c2027",
+    "url": "/static/media/flower_print.70a4c007.jpg"
+  },
+  {
+    "revision": "a083d63bbb59adf977934a84b7c6644d",
+    "url": "/static/media/ground_glass.a083d63b.jpg"
+  },
+  {
+    "revision": "88b4fc6e3b20f9033357b8bbff4d732a",
+    "url": "/static/media/ikeda.88b4fc6e.jpg"
+  },
+  {
+    "revision": "253941c0e98168a933ddb7a2f339d424",
+    "url": "/static/media/negative_carrier.253941c0.jpg"
+  },
+  {
+    "revision": "acbd7ff5e788d6ee262fe9b0f26b8d21",
+    "url": "/static/media/pin_registration.acbd7ff5.jpg"
+  },
+  {
+    "revision": "9660ed006232099db7a516c8a72c1d0c",
+    "url": "/static/media/portfolio.9660ed00.jpg"
+  },
+  {
+    "revision": "bed8223cabca2b7e11d78879c11da521",
+    "url": "/static/media/x.bed8223c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dokasphotos.com/precache-manifest.0dc307adb78446ad8c5c1a8e01c716b2.js"
+  "/precache-manifest.66c717757f764c9054bb731cfab4696a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dokasphotos.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
